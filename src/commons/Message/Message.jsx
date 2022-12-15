73f2bd03cd6a395210718abc5c5b87d723babb03
mauricio <jourdanmauricio@gmail.com>
2022-12-14 23:46:35 -0300

    Fix Meli Callback

--- src/commons/Message/Message.jsx
+++ src/commons/Message/Message.jsx
@@ -1,10 +1,25 @@
+import {useState} from 'react';
 import {FaRegWindowClose} from 'react-icons/fa';
 import styles from './message.module.css';
 
 const Message = ({msg, closeMessage}) => {
+	const [showMessage, setShowMessage] = useState(true);
+	const mountedStyle = {animation: 'inAnimation 250ms ease-in'};
+	const unmountedStyle = {
+		animation: 'outAnimation 270ms ease-out',
+		animationFillMode: 'forwards',
+	};
+
+	const handleclose = () => {
+		setShowMessage(false);
+		closeMessage();
+	};
+
 	return (
-		<div className={styles.message}>
-			<button className={styles.message__close} onClick={closeMessage}>
+		<div
+			style={showMessage ? mountedStyle : unmountedStyle}
+			className={styles.message}>
+			<button className={styles.message__close} onClick={handleclose}>
 				<FaRegWindowClose />
 			</button>
 
